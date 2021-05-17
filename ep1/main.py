e10d6a15e1c57235b32fa419fc2b50653f6577eb
AvocadoManYT <75690189+AvocadoManYT@users.noreply.github.com>
2021-05-17 10:22:40 -0700

    Update main.py

--- ep1/main.py
+++ ep1/main.py
@@ -1,1 +1,15 @@
+import random
+import discord
+from discord.ext import commands
 
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+async def on_ready():
+    print(f"{client.user} is ready")
+
+@client.command()
+async def test(ctx):
+    await ctx.send("Hi!")
+
+client.run(#bot token)
